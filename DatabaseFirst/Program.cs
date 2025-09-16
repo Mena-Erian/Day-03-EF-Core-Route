bb59a9138334d8146fef741e35b827b3065f822b
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-16 20:08:06 +0300

    Database First - EfCore Power Tools

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseFirst.Data;
+using HelperUtilities;
+
+namespace DatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new NorthwindContext();
+
+            foreach (var prod in context.Products)
+            {
+                prod.ProductName.Print();
+            }
+        }
+    }
+}
